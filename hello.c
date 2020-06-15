aa73a6a52b4db365e60d78337654412b066bb0a5
Surya Jatavallabhula <suja3865@colorado.edu>
2020-06-14 19:34:02 -0600

    Step 10-11

--- hello.c
+++ hello.c
@@ -2,5 +2,10 @@
 
 int main() {
 	printf("Hello, World!\n");
+	/* Here's a joke:
+	   The three most dangerous things in the world are
+	   a programmer with a soldering iron, a hardware
+	   engineer with a software patch, and a user with
+           an idea.  – The Wizardry Compiled by Rick Cook*/
 	return 0;
 }
